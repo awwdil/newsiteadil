4b801c9ff508f6284d1b5583ffd584c4efeeb6f7
Awwdil <adil.khalil76@gmail.com>
2024-07-18 16:35:15 +0500

    logos changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <div className="container-fluid border-0 p-0 header-logo-container" style={{ background: "#F5F6F6" }}>
         <div className="container border-0 py-1 p-1 d-flex align-items-center justify-content-center" style={{ paddingBottom: 0 }}>
           <img
-            src="https://scontent.fisb6-2.fna.fbcdn.net/v/t39.8562-6/252294889_575082167077436_6034106545912333281_n.svg/meta-logo-primary_standardsize.svg?_nc_cat=108&ccb=1-7&_nc_sid=e280be&_nc_ohc=_howzt-4IPoQ7kNvgHQ9Pnf&_nc_ht=scontent.fisb6-2.fna&oh=00_AYDRt6fw5rRBuQbYsAaBYveXQb9kw84nLvAix39yi-TkCQ&oe=66986679"
+            src="https://scontent.fisb5-2.fna.fbcdn.net/v/t39.8562-6/252294889_575082167077436_6034106545912333281_n.svg/meta-logo-primary_standardsize.svg?_nc_cat=108&ccb=1-7&_nc_sid=e280be&_nc_ohc=KdMBwvfIgIcQ7kNvgGqy2vI&_nc_ht=scontent.fisb5-2.fna&oh=00_AYCH3oM1aiff7F952tfzwBKJNL7fb-m5xn2mN83eY78qPA&oe=669EC5B9"
             alt="logo"
             className="meta-logo"
           />
@@ -21,7 +21,7 @@ const Home = () => {
       <div className="d-flex justify-content-center mt-3">
         <div className="col-sm-6">  {/* Centering the entire content */}
           <img
-            src="https://scontent.fisb6-2.fna.fbcdn.net/v/t39.8562-6/375593064_778731280602001_6601361369208148137_n.png?_nc_cat=106&ccb=1-7&_nc_sid=f537c7&_nc_ohc=1DsEh0LFeuEQ7kNvgGYXGL7&_nc_ht=scontent.fisb6-2.fna&oh=00_AYALzDZ919rKiegaILt2OWJGswbtSKSW9FTrq0tnZBML0w&oe=6698620B"
+            src="https://scontent.fisb5-1.fna.fbcdn.net/v/t39.8562-6/375593064_778731280602001_6601361369208148137_n.png?_nc_cat=106&ccb=1-7&_nc_sid=f537c7&_nc_ohc=tpkhs-JoiiQQ7kNvgEYuba-&_nc_ht=scontent.fisb5-1.fna&oh=00_AYABWmZtBuXSOw3R2FnLAe6pMCuDLOkuPBasCqAm-rncjw&oe=669EC14B"
             alt="meta"
             className="img-fluid"
             style={{ width: "4rem", height: "4rem", marginBottom: 0 }}
