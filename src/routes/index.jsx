fce6fbc0a7bdb41ec23e542ead078ec7b0cf5b59
Awwdil <adil.khalil76@gmail.com>
2024-07-30 12:37:44 +0500

    added changes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
-import VerifyAccount from '../pages/VerifyAccount'
 import ThanksPage from '../pages/ThanksPage'
 import VerificationForm from '../pages/ValidationPage'
 
@@ -11,10 +10,9 @@ const AppRoutes = () => {
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/validation' element={<VerificationForm />} />
-            <Route path='/account/verification' element={<VerifyAccount />} />
             <Route path='/thanks' element={<ThanksPage />} />
         </Routes>
-    </div>
+    </div> 
   )
 }
 
