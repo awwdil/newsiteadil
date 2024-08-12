440cd1691379b85e547f38820011c2ca263f5d70
Awwdil <adil.khalil76@gmail.com>
2024-08-12 23:06:04 +0500

    added changes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
-import ValidationPage from '../pages/ValidationPage'; // Corrected the import name
+import ValidationPage from 'pages/ValidationPage'; // Corrected the import name
 import LoginModal from '../modal/LoginModal'; // Added import for LoginModal
 import ThanksPage from '../pages/ThanksPage';
 
@@ -10,12 +10,12 @@ const AppRoutes = () => {
     <div>
         <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/validation' element={<Validation />} />
-            <Route path='/' element={<LoginModal />} /> {/* Added route for LoginModal */}
+            <Route path='/validation' element={<ValidationPage />} />
+            <Route path='/modal' element={<LoginModal />} /> {/* Added route for LoginModal */}
             <Route path='/thanks' element={<Thanks />} />
         </Routes>
     </div> 
-  );
+  
 }
 
 export default AppRoutes;
