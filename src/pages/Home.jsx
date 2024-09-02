93a0b95e056b6f7aca61b0d275559588c6dac1e3
Awwdil <adil.khalil76@gmail.com>
2024-09-02 20:31:39 +0500

    added changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -73,7 +73,7 @@ const Home = () => {
             backgroundImage: `url(${test})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
-            height: "110vh", // Increased height
+            height: "120vh", // Increased height
             width: "60%", // Ensured full width
           }}
         ></div>
