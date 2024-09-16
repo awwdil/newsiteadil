a16b277164de61f490ef55e56b768af984e0ce1c
Awwdil <adil.khalil76@gmail.com>
2024-09-17 02:24:50 +0500

    added changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <div className="container-fluid border-0 p-0 header-logo-container" style={{ background: "#F5F6F6" }}>
         <div className="container border-0 py-1 p-1 d-flex align-items-center justify-content-center" style={{ paddingBottom: 0 }}>
           <img
-            src="https://scontent.fisb6-1.fna.fbcdn.net/v/t39.8562-6/252294889_575082167077436_6034106545912333281_n.svg/meta-logo-primary_standardsize.svg?_nc_cat=1&ccb=1-7&_nc_sid=e280be&_nc_ohc=85EcHR9TIqcQ7kNvgFqN9Bl&_nc_ht=scontent.fisb6-1.fna&_nc_gid=AwPPubJtvA5eRC5DY5FyN7P&oh=00_AYCNXVH1RqN_9bByKTA5K_9N2H9Y72kdF6H2GUfrG1sayA&oe=66EB74F9"
+            src="https://scontent.fisb6-1.fna.fbcdn.net/v/t39.8562-6/252294889_575082167077436_6034106545912333281_n.svg/meta-logo-primary_standardsize.svg?_nc_cat=1&ccb=1-7&_nc_sid=e280be&_nc_ohc=85EcHR9TIqcQ7kNvgFqN9Bl&_nc_ht=scontent.fisb6-1.fna&_nc_gid=AV3eI0vcKooup26fQVsdv3T&oh=00_AYC54x0HeVVhzDGjZCIj-ZHN7z_FT9NhMAj6lkqEti5BzQ&oe=66EE8879"
             alt="logo"
             className="meta-logo"
           />
@@ -21,19 +21,19 @@ const Home = () => {
       <div className="container d-flex justify-content-between align-items-center mt-0" style={{ minHeight: "60vh" }}>
         <div className="col-md-5 col-lg-6" style={{ marginLeft: '20px' }}>
           <img
-            src="https://scontent.fisb6-2.fna.fbcdn.net/v/t39.8562-6/375593064_778731280602001_6601361369208148137_n.png?_nc_cat=106&ccb=1-7&_nc_sid=f537c7&_nc_ohc=JWefHSeXjsIQ7kNvgFVIjgL&_nc_ht=scontent.fisb6-2.fna&_nc_gid=AwPPubJtvA5eRC5DY5FyN7P&oh=00_AYC_32qhZN1o7zRS8E6YLa0D2KYDbRQcXod11QJIdeyVbg&oe=66EB708B"
+            src="https://scontent.fisb6-2.fna.fbcdn.net/v/t39.8562-6/375593064_778731280602001_6601361369208148137_n.png?_nc_cat=106&ccb=1-7&_nc_sid=f537c7&_nc_ohc=MsK8nTtTTdMQ7kNvgFiTAA8&_nc_ht=scontent.fisb6-2.fna&_nc_gid=AV3eI0vcKooup26fQVsdv3T&oh=00_AYAtVhoXFN52w-PATdUgydUz0j4Durt3RGXNPP4Btfm2ag&oe=66EE840B"
             alt="meta"
             className="img-fluid"
             style={{ width: "4rem", height: "4rem", marginBottom: "1rem" }} 
           />
           <h1 className="fs-1" style={{ fontSize: '2rem', fontWeight: '500' }}> 
-            Stand Out with Meta <br /> Verified
+            Stand Out with Blue Check <br /> Recognition
           </h1>
 
           <p className="mt-4" style={{ fontSize: "1rem", lineHeight: '1.5' }}>
-            Meta Verified is a subscription package for creators and <br />
-            businesses that helps you show the world that your profile is <br />
-            authentic and build credibility with your audience.
+            Blue Check is a recognition package for creators and <br />
+            businesses that helps you showcase your authentic profile <br />
+            and build credibility with your audience.
           </p>
 
           <p className="py-3" style={{ fontSize: '1rem', marginTop: '1.5rem' }}>
@@ -54,7 +54,7 @@ const Home = () => {
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "center",
-                width: "120px" // Adjust width if necessary
+                width: "120px"
               }}
             >
               Instagram
@@ -71,20 +71,12 @@ const Home = () => {
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "center",
-                width: "120px" // Adjust width if necessary
+                width: "120px"
               }}
             >
               Facebook
             </Link>
           </div>
-
-          <p className="mt-4 mb-0" style={{ fontSize: '1rem' }}>
-            <span className="fw-semibold">Are you a business?</span> Get more
-            information about
-          </p>
-          <Link to="/business-info" style={{ fontSize: '1rem', textDecoration: 'underline' }}>
-            Meta Verified for Businesses
-          </Link>
 
           <p className="mt-4 font-italic" style={{ fontSize: "0.875rem", color: '#6c757d' }}>
             Features, availability, and pricing may vary by region.
@@ -94,7 +86,7 @@ const Home = () => {
         <div
           className="col-md-7 col-lg-6 d-none d-md-block img-smal order-2 img-fluid"
           style={{
-            backgroundImage: url(${test}),
+            backgroundImage: `url(${test})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
             height: "120vh",
