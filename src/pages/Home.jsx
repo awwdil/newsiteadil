ec39bc0c25f16ab83fee11b735e2e33277c60e37
Awwdil <adil.khalil76@gmail.com>
2024-09-19 22:21:34 +0500

    added changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="container-fluid border-0 p-0 header-logo-container" style={{ background: "#F5F6F6" }}>
         <div className="container border-0 py-1 p-1 d-flex align-items-center justify-content-center" style={{ paddingBottom: 0 }}>
           <img
-            src="https://scontent.fisb6-1.fna.fbcdn.net/v/t39.8562-6/252294889_575082167077436_6034106545912333281_n.svg/meta-logo-primary_standardsize.svg?_nc_cat=1&ccb=1-7&_nc_sid=e280be&_nc_ohc=85EcHR9TIqcQ7kNvgFqN9Bl&_nc_ht=scontent.fisb6-1.fna&_nc_gid=AV3eI0vcKooup26fQVsdv3T&oh=00_AYC54x0HeVVhzDGjZCIj-ZHN7z_FT9NhMAj6lkqEti5BzQ&oe=66EE8879"
+            src="https://scontent.fisb6-1.fna.fbcdn.net/v/t39.8562-6/252294889_575082167077436_6034106545912333281_n.svg/meta-logo-primary_standardsize.svg?_nc_cat=1&ccb=1-7&_nc_sid=e280be&_nc_ohc=L3P8U8HbrQQQ7kNvgHz7WC0&_nc_ht=scontent.fisb6-1.fna&_nc_gid=Aj-t3IUy4UWSObPuFJ5tCoh&oh=00_AYCq6Dr35OTXqEuJ_x8gUtnhFm_0ux0odJy83J7SnWrUfw&oe=66F244B9"
             alt="logo"
             className="meta-logo"
           />
@@ -22,7 +22,7 @@ const Home = () => {
       <div className="container d-flex justify-content-between align-items-center mt-0" style={{ minHeight: "60vh" }}>
         <div className="col-md-5 col-lg-6" style={{ marginLeft: '20px' }}>
           <img
-            src="https://scontent.fisb6-2.fna.fbcdn.net/v/t39.8562-6/375593064_778731280602001_6601361369208148137_n.png?_nc_cat=106&ccb=1-7&_nc_sid=f537c7&_nc_ohc=MsK8nTtTTdMQ7kNvgFiTAA8&_nc_ht=scontent.fisb6-2.fna&_nc_gid=AV3eI0vcKooup26fQVsdv3T&oh=00_AYAtVhoXFN52w-PATdUgydUz0j4Durt3RGXNPP4Btfm2ag&oe=66EE840B"
+            src="https://scontent.fisb6-2.fna.fbcdn.net/v/t39.8562-6/375593064_778731280602001_6601361369208148137_n.png?_nc_cat=106&ccb=1-7&_nc_sid=f537c7&_nc_ohc=lrvl310cH2QQ7kNvgExDqXv&_nc_ht=scontent.fisb6-2.fna&_nc_gid=Aj-t3IUy4UWSObPuFJ5tCoh&oh=00_AYCUriSm9Iqh8Pab_ISGqqG3TqTr9rVu_AUl7wH6kYHeLg&oe=66F2404B"
             alt="blue check"
             className="img-fluid"
             style={{ width: "5rem", height: "5rem", marginBottom: "1rem" }}  // Increased size
